ef78611c12e7ba471bc42781c1c0fb21db085fc2
syed zohaib <zohaibsyed914@gmail.com>
2024-10-31 00:49:25 +0500

    working

---
+++ src/components/Section.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section() {
+    return (
+        <Wrap></Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+width:100vw;
+height:100vh;
+background-image: url("./images/bg1.avif");
+background-size:cover;
+background-position:center;
+`
